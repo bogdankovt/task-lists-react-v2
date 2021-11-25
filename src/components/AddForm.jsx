a1b5909e1df71b396c319308153c86e65921e63c
Bohdan Kovtun <bohdankovt@tahometer.com>
2021-11-25 10:00:14 +0200

    add date today to add form

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const AddForm = ({...props}) => {
 
-
-    props.task = {title: '', desc:'', dueDate:''}
+    const dateToday = new Date().toISOString().split('T')[0];
+    props.task = {title: '', desc:'', dueDate: dateToday}
 
     const submitHandle = (e) => {
         e.preventDefault();
@@ -19,27 +19,27 @@ const AddForm = ({...props}) => {
         
                         <div className="add-form mt-2">
                             <form name="taskEditForm" onSubmit={submitHandle}>
-                            <div className="input-group">
-                                <div className="input-group-prepend">
-                                  <i className="bi bi-fonts input-group-text" id="taskNewTitle"></i>
+                                <div className="input-group">
+                                    <div className="input-group-prepend">
+                                    <i className="bi bi-fonts input-group-text" id="taskNewTitle"></i>
+                                    </div>
+                                    <input name="title" onChange={setField} type="text" className="form-control"  placeholder="Title"></input>
                                 </div>
-                                <input name="title" onChange={setField} type="text" className="form-control"  placeholder="Title"></input>
-                            </div>
-                            <div className="input-group mt-2">
-                                <div className="input-group-prepend">
-                                  <i className="bi bi-type input-group-text" id="taskNewDesc"></i>
+                                <div className="input-group mt-2">
+                                    <div className="input-group-prepend">
+                                    <i className="bi bi-type input-group-text" id="taskNewDesc"></i>
+                                    </div>
+                                    <input name="desc" onChange={setField} type="text" className="form-control" aria-describedby="taskNewDesc" placeholder="Description"></input>
                                 </div>
-                                <input name="desc" onChange={setField} type="text" className="form-control" aria-describedby="taskNewDesc" placeholder="Description"></input>
-                            </div>
-                            <div className="input-group mt-2">
-                                <div className="input-group-prepend">
-                                  <i className="bi bi-calendar-date input-group-text" id="taskNewExpDate"></i>
+                                <div className="input-group mt-2">
+                                    <div className="input-group-prepend">
+                                    <i className="bi bi-calendar-date input-group-text" id="taskNewExpDate"></i>
+                                    </div>
+                                    <input name="dueDate" defaultValue={dateToday} onChange={setField} type="date" className="form-control edit-exp-date" aria-describedby="taskNewExpDate" placeholder="Expiration date"></input>
                                 </div>
-                                <input name="dueDate" onChange={setField} type="text" className="form-control edit-exp-date" aria-describedby="taskNewExpDate" placeholder="Expiration date"></input>
-                            </div>
-                            <button className="btn btn-sm btn-success mt-4 task-save-button w-100">Create New</button>
+                                <button className="btn btn-sm btn-success mt-4 task-save-button w-100">Create New</button>
 
-                        </form>
+                            </form>
                         </div>
     )
 }
