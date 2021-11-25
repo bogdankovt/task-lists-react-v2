d3cb3f0aa56b013f67288c87d788b5526e0ce35e
Bohdan Kovtun <bohdankovt@tahometer.com>
2021-11-25 18:09:23 +0200

    add sidebar link routes

--- src/App.js
+++ src/App.js
@@ -8,74 +8,74 @@ import ListTasksService from "./ListTasksService";
 
 function App() {
 
-  const [lists, setLists] = useState([]);
+  
   const [activeList, setActiveList] = useState({title: "List not selected"});
   const [lastUpdate, setLastUpdate] = useState({});
   const [modalData, setModalData] = useState({});
 
-  const updateLists = () => {
-    ListTasksService.getAll()
-      .then(res => {
-        setLists(res.lists)
-      })
-  }
+  // const updateLists = () => {
+  //   ListTasksService.getAll()
+  //     .then(res => {
+  //       setLists(res.lists)
+  //     })
+  // }
 
-  const updateActiveList = (listId) => {
-    ListTasksService.getListById(listId)
-      .then(res => {
-        setActiveList(res);
-      })
-  }
+  // const updateActiveList = (listId) => {
+  //   ListTasksService.getListById(listId)
+  //     .then(res => {
+  //       setActiveList(res);
+  //     })
+  // }
 
-  useEffect(() => {
-    updateLists()
-  }, []);
+  // useEffect(() => {
+  //   updateLists()
+  // }, []);
   
 
-  useEffect(() => {
-    if(activeList.listId) {
-      updateActiveList(activeList.listId)
-      updateLists()
-    }
-  }, [lastUpdate])
+  // useEffect(() => {
+  //   if(activeList.listId) {
+  //     updateActiveList(activeList.listId)
+  //     updateLists()
+  //   }
+  // }, [lastUpdate])
 
-  function trigerUpdate() {
-    setLastUpdate(new Date().getTime());
-  }
+  // function trigerUpdate() {
+  //   setLastUpdate(new Date().getTime());
+  // }
 
   
 
-  const changeActiveList = (listId) => {
-    updateActiveList(listId)
-  }
+  // const changeActiveList = (listId) => {
+  //   updateActiveList(listId)
+  // }
 
-  const addNewTask = (t) => {
-    if(activeList.listId) {
-      ListTasksService.createTaskForList(activeList.listId, t)
-      .then(trigerUpdate);
-      }
-    }
+  // const addNewTask = (t) => {
+  //   if(activeList.listId) {
+  //     ListTasksService.createTaskForList(activeList.listId, t)
+  //     .then(trigerUpdate);
+  //     }
+  //   }
 
 
-  const deleteTask = (taskId) => {
-    ListTasksService.removeTask(taskId)
-    .then(trigerUpdate)
-  }
+  // const deleteTask = (taskId) => {
+  //   ListTasksService.removeTask(taskId)
+  //   .then(trigerUpdate)
+  // }
 
-  const editTask = (t) => {
-    ListTasksService.updateTask(t)
-    .then(trigerUpdate)
-  }
+  // const editTask = (t) => {
+  //   ListTasksService.updateTask(t)
+  //   .then(trigerUpdate)
+  // }
   
   return (
     
       <div className="content card flex-row ">
-        <Sidebar onClick={changeActiveList} lists={lists}/>
-        <div className="selected-list">
+        <Sidebar/>
+        {/* <div className="selected-list">
           <ListTasks activeList={activeList} onDelete={deleteTask} onEdit={(s) => setModalData(s)}/>
           <AddForm onSubmit={addNewTask}/>
         </div> 
-        <EditTaskModal onSubmit={editTask} data={modalData}/>
+        <EditTaskModal onSubmit={editTask} data={modalData}/> */}
       </div>
     
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,25 +1,40 @@
 import React from "react";
-import listTasksService from "../ListTasksService";
+import ListTasksService from "../ListTasksService";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
+const Sidebar = () => {
+    const [lists, setLists] = useState([]);
 
+    const updateLists = () => {
+      ListTasksService.getAll()
+        .then(res => {
+          setLists(res.lists)
+        })
+    }
 
-const Sidebar = ({lists, ...props}) => {
+    useEffect(() => {
+      updateLists()
+    }, []);
 
-    const changeActiveList = (lisId) => {
-        props.onClick(lisId);
-    }  
+    // const changeActiveList = (lisId) => {
+    //     props.onClick(lisId);
+    // }  
     return (
       <div className="sidebar">
          
 
             <ul className="list-group w-100">
+                <li className=" text-white list-group-item list-group-item-action d-flex justify-content-between align-items-center bg-primary ">
+                  <Link to={'/today'} className="text-white">Tasks For Today</Link>
+                </li>
                 {lists.map(l => 
     
-                    <li key={l.listId} onClick={() => changeActiveList(l.listId)} className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                      {l.title}
+                    <li key={l.listId} className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                      <Link to={`/lists-tasks/${l.listId}`} activeClassName="current" >{l.title}</Link>
                       {l.countNotDoneTask ? <span className="badge badge-primary badge-pill">{l.countNotDoneTask}</span> : ''}
                     </li>
-
+                    
                 )}
               
             </ul>
